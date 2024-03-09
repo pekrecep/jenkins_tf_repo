1f647e5f33d302bc528a87e72d042797c9561b89
Rene Audain <rene.audain@gmail.com>
2024-03-09 14:34:11 -0600

    update

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "account_id" {
+  type = string
+  default = "503271535804"
+}
+variable "bucket_name" {
+  type = string
+  default = "jenkins-test-ra2282024"
+}
+variable "aws_linux_ami" {
+  type = string
+  default = "ami-0440d3b780d96b29d"
+}
+variable "key-pair-name" {}
